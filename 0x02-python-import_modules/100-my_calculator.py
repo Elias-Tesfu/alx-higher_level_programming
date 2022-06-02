5355368e3582955573e949c0141172a0d0318380
Elias Tefsu <eliyastesfu@gmail.com>
2022-06-02 20:56:21 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     argv = sys.argv[1:]
-    arg_count = len(argv)
+    argv_count = len(argv)
     operators = ["+", "-", "*", "/"]
-    if arg_count != 3:
+    if argv_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif sys.argv[2] != operators:

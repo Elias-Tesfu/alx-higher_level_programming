1ead5022ab7b1b2b66e11ca5e10589b161fbcb48
Elias Tesfu <eliyastesfu@gmail.com>
2022-06-06 11:33:06 -0700

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return my_list
-    list = my_list
-    list[idx] = element
-    return list
+    temp = list(my_list)
+    temp[idx] = element
+    return temp

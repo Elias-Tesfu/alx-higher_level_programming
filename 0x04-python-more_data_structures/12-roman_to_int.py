450189051b320d01d007c78b857f50e7868070bd
Elias Tesfu <eliyastesfu@gmail.com>
2022-06-08 09:52:58 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
     num = 0
     for i in range(len(roman_string)):
         if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
-            num += roman[roman_strin[i]] - 2 * roman[roman_string[i - 1]]
+            num += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
         else:
             num += roman[roman_string[i]]
-    return (integer)
+    return (num)

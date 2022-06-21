f20129b14891b4e722c80209bb2108e8f6b36b73
Elias Tesfu <eliyastesfu@gmail.com>
2022-06-21 11:46:14 -0700

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+""" A class square that defines a square
+"""
+
+
 class Square:
+    """ Empty class with size private att
+    """
     def __init__(self, size=0):
+        """ 
+            Instantation with size
+        Args: 
+            size: size of square
+        """
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:

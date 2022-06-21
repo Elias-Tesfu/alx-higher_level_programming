6e892104bcc94539b93f9f900416e67af0e5633c
Elias Tesfu <eliyastesfu@gmail.com>
2022-06-21 11:43:51 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+""" A class square that defines a square
+"""
+
+
 class Square:
+    """ Empty class square with size private attribute
+    """
     def __init__(self, size):
+        """
+            Instantation with size
+        Args:
+            size: size of the square
+        """
         self.__size = size
